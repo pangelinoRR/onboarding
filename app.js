e0746e741e074fc9e5365aaf127464c98798df53
Runtime-Revolution <runtime-revolution@RuntimetionsMBP.Home>
2024-04-12 14:51:17 +0100

    added base router and routes

--- app.js
+++ app.js
@@ -4,3 +4,22 @@
  * Connects to the database.
  */
 require("./config");
+
+const express = require("express");
+
+const app = express();
+
+/**
+ * Allows express to parse json body data.
+ */
+app.use(express.json());
+
+/**
+ * Imports all routes.
+ */
+app.use(require("./routes"));
+
+/**
+ * Starts the app.
+ */
+app.listen(process.env.PORT || 3000);
